bd19bf12471f6ef20ad0bcdc5af159cf1ada8e86
Endersky78 <36282715+Endersky78@users.noreply.github.com>
2020-10-10 23:28:37 -0700

    Create VRLocomotionController.cs

---
+++ 2020/VRLocomotionController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour {
+
+    public XRController leftTeleportRay;
+    public XRController rightTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.1f;
+
+    void Update() {
+        if(leftTeleportRay) {
+            leftTeleportRay.gameObject.SetActive(CheckIfActivated(leftTeleportRay));
+        }
+
+        if (rightTeleportRay) {
+            rightTeleportRay.gameObject.SetActive(CheckIfActivated(rightTeleportRay));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller) {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated);
+        return isActivated;
+    }
+
+    //Teleportation area is free movement while teleportation anchor is one specific spot you can go to
+}
