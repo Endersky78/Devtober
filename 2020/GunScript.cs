d532852d9ab9a577c7f15f4d0ce847903bf71edc
Endersky78 <36282715+Endersky78@users.noreply.github.com>
2020-10-11 14:27:01 -0700

    Create GunScript.cs

---
+++ 2020/GunScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Not actually going to be used for game, just kinda wanted to make the testing more fun and this was easy to make
+public class GunScript : MonoBehaviour {
+    public float speed = 40;
+    public GameObject bullet;
+    public Transform barrel;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    public void Fire() {
+        GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
+        spawnedBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
+        audioSource.PlayOneShot(audioClip);
+        Destroy(spawnedBullet, 2);
+    }
+}
